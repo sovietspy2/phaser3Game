cba7dcfb3c4755cf722640511315bfdc6ca71936
SovietSpy2 <sovietspy2@yandex.com>
2018-11-25 22:19:55 +0000

    added new character model

--- src/models/player.js
+++ src/models/player.js
@@ -1,5 +1,5 @@
 export default class Player {
-    constructor(scene, x, y) {
+    constructor(scene) {
         this.scene = scene;
 
         const spawnPoint = this.scene.map.findObject("Objects", obj => obj.name === "spawn");
@@ -7,29 +7,18 @@ export default class Player {
         // anims
         const anims = scene.anims;
 
-        anims.remove("player-idle");
-        anims.remove("player-run");
-        anims.create({
-          key: "player-idle",
-          frames: anims.generateFrameNumbers("player", { start: 0, end: 3 }),
-          frameRate: 3,
-          repeat: Phaser.FOREVER
-        });
-        anims.create({
-          key: "player-run",
-          frames: anims.generateFrameNumbers("player", { start: 8, end: 15 }),
-          frameRate: 12,
-          repeat: Phaser.FOREVER
-        });
-
         // player creating
         this.score = 0;
 
+        // jumping state
+        this.jumping = false;
+
         this.sprite = scene.physics.add
-            .sprite(spawnPoint.x,spawnPoint.y, "player", 0)
+            .sprite(spawnPoint.x,spawnPoint.y, "hero-idle", 0)
             .setDrag(1000,0)
             .setMaxVelocity(300,400)
-            .setSize(20,20) // hitbox size
+            .setSize(20,50) // hitbox size
+            .setOffset(10, 0)
             .setCollideWorldBounds(true);
 
         const { LEFT, RIGHT, UP, W, A, D } = Phaser.Input.Keyboard.KeyCodes;
@@ -46,37 +35,62 @@ export default class Player {
     }
 
     update() {
+
         const keys = this.keys;
         const sprite = this.sprite;
         const onGround = sprite.body.blocked.down;
         const acceleration = onGround ? 600 : 200;
-    
+
+
+
+        //debugger;
         // Apply horizontal acceleration when left/a or right/d are applied
         if (keys.left.isDown || keys.a.isDown) {
           sprite.setAccelerationX(-acceleration);
           // No need to have a separate set of graphics for running to the left & to the right. Instead
           // we can just mirror the sprite.
           sprite.setFlipX(true);
+          
+          if (onGround) {
+            this.sprite.setOffset(20, 0);
+          }
+         
+          
         } else if (keys.right.isDown || keys.d.isDown) {
           sprite.setAccelerationX(acceleration);
           sprite.setFlipX(false);
+          if (onGround) {
+            this.sprite.setOffset(25, 0);
+          }
+         
         } else {
-          sprite.setAccelerationX(0);
+          sprite.setAccelerationX(0);   
         }
     
         // Only allow the player to jump if they are on the ground
-        if (onGround && (keys.up.isDown || keys.w.isDown)) {
-          sprite.setVelocityY(-500);
-        }
+      
     
         // Update the animation/texture based on the state of the player
         if (onGround) {
-          if (sprite.body.velocity.x !== 0) sprite.anims.play("player-run", true);
-          else sprite.anims.play("player-idle", true);
-        } else {
-          sprite.anims.stop();
-          sprite.setTexture("player", 10);
+          if ((keys.up.isDown || keys.w.isDown)) {
+            sprite.setVelocityY(-500);
+            if (sprite.flipX) {
+              this.sprite.setOffset(10, 10);
+            } else {
+              this.sprite.setOffset(30, 10);
+            }
+            sprite.anims.play("hero-jump", true);
+          } else if (sprite.body.velocity.x !== 0) {
+            sprite.anims.play("hero-run", true);
+          } else {
+            sprite.anims.play("hero-idle", true);
+            this.sprite.setOffset(10, 0);
+          }
         }
+      }
+
+      setJumpingFalse() {
+        this.jumping = false;
       }
     
       destroy() {
