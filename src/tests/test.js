ebfcc2fe57c8666f702a52aa822c3b5710aa7493
SovietSpy2 <sovietspy2@yandex.com>
2018-11-26 15:50:11 +0100

    jest

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,12 +1,10 @@
 import Player from "../models/player";
-//import Phaser from 'phaser';
+import Phaser from 'phaser';
 
 describe('Testing player model', () => {
-
+    
     //let scene = new Phaser.Scene({});
     it('It should not be null', () => {
      expect("test").toBe('test');
     });
   });
-
-  console.log("vege");
