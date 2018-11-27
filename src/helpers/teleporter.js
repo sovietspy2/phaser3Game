2b2daa2e726236996cde091a4be34a4be149e603
SovietSpy2 <sovietspy2@yandex.com>
2018-11-27 20:42:00 +0000

    now teleport object doesnt need gid

--- src/helpers/teleporter.js
+++ src/helpers/teleporter.js
@@ -1,23 +1,15 @@
 export function teleporter(scene) {
 
-    //const start = scene.map.findObject("Objects", obj => obj.name === "doorin");
-
-    let doorIn = scene.map.createFromObjects("Objects", 168, {key: "doorin"});
-    doorIn=doorIn[0];
+    const start = scene.map.findObject("Objects", obj => obj.name === "doorin");
     const doorOut = scene.map.findObject("Objects", obj => obj.name === "doorout");
 
-    doorIn = scene.physics.add.existing(doorIn); 
-    doorIn.body.allowGravity = false;
+    let doorIn = scene.physics.add.sprite(start.x, start.y, ""); // it doesnt need an image because it will never render
+    doorIn.body.allowGravity = false; 
+    doorIn.visible = false;
 
     scene.doorOut = {x: doorOut.x, y: doorOut.y }
     
-    scene.physics.add.overlap(scene.player.sprite, doorIn, teleport, null, scene)
-
-    //Phaser.Math.Distance.Between(doorIn.x, doorIn.y, player.sprite.x, player.sprite.y)
-    debugger;
-   // const secret = scene.map.findObject("Secret", obj => obj.name === "secret");
-    //const tile = scene.map.getTileAtWorldXY(secret.x, secret.y);
-    //scene.map.setCollision( [tile.x, tile.y] , )
+    scene.physics.add.overlap(scene.player.sprite, doorIn, teleport, null, scene);
 
 }
 
