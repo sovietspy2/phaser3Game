3aeb1b5d41501f5aeafce4b176c0edc397fb01e6
SovietSpy2 <sovietspy2@yandex.com>
2019-03-02 19:47:33 +0100

    refactor

--- src/helpers/createTextFromObject.js
+++ src/helpers/createTextFromObject.js
@@ -1,10 +1,10 @@
-export default function(layerName, id, scene)
+export default function(layerName, id, scene, fontSize=20)
     {
         const objects = scene.map.filterObjects(layerName, obj => obj.name === id);
         objects.forEach(item => {
-            console.log(item);
+
             if (item.visible) {
-                scene.add.text(item.x, item.y, item.text.text).setColor(item.text.color).setFontSize(20);
+                scene.add.text(item.x, item.y, item.text.text).setColor(item.text.color).setFontSize(fontSize);
             }
         
         });
